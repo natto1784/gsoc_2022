49eaba93c9eef67c81accd15d77d3e1a12635d87
Amneesh Singh <natto@weirdnatto.in>
2022-04-29 23:11:38 +0530

    initial solution
    can be refined
    
    Signed-off-by: Amneesh Singh <natto@weirdnatto.in>

--- wordstat.c
+++ wordstat.c
@@ -1,4 +1,5 @@
 #include "wordstat.h"
+#include <stdio.h>
 
 /*
  * This should hold everything that is needed to keep the working state.  Feel
@@ -6,36 +7,66 @@
  * code compiles.
  */
 struct _WordStat {
-    int unused_delete_me;
+    char **words;
+    char *delim;
+    bool regex;
 };
 
 /*
  * Returns new structure that will be used when calling any APIs in this file.
  */
 WordStat *
-WordStatNew(const char *delim G_GNUC_UNUSED,
-            bool regex G_GNUC_UNUSED)
+WordStatNew(const char *delim,
+            bool regex)
 {
-    return NULL;
+    if(!delim || delim[0]==0)
+        return NULL;
+    
+    WordStat *ws = g_new(WordStat, 1);
+
+    ws->words = NULL;
+    ws->delim = g_strdup(delim);
+    ws->regex = regex;
+    
+    return ws;
 }
 
 /*
  * Frees all the memory that was allocated for @ws and all its members during
  * the lifetime of the structure.
  */
-void WordStatFree(WordStat *ws G_GNUC_UNUSED)
+void WordStatFree(WordStat *ws)
 {
+    if(!ws)
+        return;
+
+    g_strfreev(ws->words);
+    g_free(ws->delim);
+
+    g_free(ws);
+
+    return;
 }
 
 /*
  * Splits the @text into words and updates @ws accordingly.
  */
 void
-WordStatAddText(WordStat *ws G_GNUC_UNUSED,
-                const char *text G_GNUC_UNUSED)
+WordStatAddText(WordStat *ws,
+                const char *text)
 {
-}
+   char *lower_text = g_ascii_strdown (text, -1);
 
+   if (ws->regex)
+        ws->words = g_regex_split_simple(ws->delim, lower_text, 0, 0);
+   else {
+        ws->words = g_strsplit_set(lower_text, ws->delim, 0);
+    }
+
+    g_free(lower_text);
+
+    return;
+  }
 /*
  * Finds the most frequent word and returns it as a string.  Caller owns the
  * string and is responsible for freeing it.  If @num is supplied, the variable
@@ -43,17 +74,60 @@ WordStatAddText(WordStat *ws G_GNUC_UNUSED,
  * returned word.
  */
 char *
-WordStatGetMostFrequent(WordStat *ws G_GNUC_UNUSED,
-                        size_t *num G_GNUC_UNUSED)
+WordStatGetMostFrequent(WordStat *ws,
+                        size_t *num)
 {
-    return NULL;
+    char *word = NULL;
+    int mx = 0;
+
+    if(!ws || !(ws->words)) goto done;
+    
+    char **words = g_strdupv(ws->words);
+    char **bkp = words;
+
+    while (*bkp != NULL) {
+      if ((*bkp)[0]) {
+        int c = 1;
+          
+        for (char **cur = bkp + 1; *cur; cur++) {
+          if (!g_strcmp0(*bkp, *cur))
+            c++, *cur = "";
+        }
+        
+        if(mx < c)
+            mx = c, word = strdup(*bkp);
+      }
+          
+      bkp++;
+    }
+
+    //    g_strfreev(words);
+
+ done:
+    if(num)
+        *num = mx;
+    
+    return word;
 }
 
 /*
  * Returns the total amount of words added.
  */
 size_t
-WordStatGetTotal(WordStat *ws G_GNUC_UNUSED)
+WordStatGetTotal(WordStat *ws)
 {
-    return 0;
+    if(!ws || !(ws->words))
+        return 0;
+
+    char **tmp = ws->words;
+
+    int c = 0;
+    
+    while (*tmp) {
+        if ((*tmp)[0])
+            c++;
+        tmp++;
+    }
+    
+    return c;
 }
